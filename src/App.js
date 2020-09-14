5948f40933e6cd5439f3e1a11d76660dd45da43a
Lukey <ldipaola66@gmail.com>
2020-09-14 22:43:38 +1000

    added filter functions for search and dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import employees from './employees.json';
+import React, {useState, useEffect} from 'react';
+import Employees from './employees.json';
 import EmployeeCard from './compenents/EmployeeCard'
 import SearchBar from './compenents/SearchBar'
 import Wrapper from './compenents/Wrapper'
 import Title from './compenents/Title'
+import DropDown from './compenents/DropDown'
 
 function App() {
-  const [employee, setEmployee] = React.useState(employees);
+  const [employees, setEmployees] = useState(Employees);
+  const [search, setSearch] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
-  console.log(employee);
+  const handleChange = event => {
+    setSearch(event.target.value);
+  }; 
+  const handleOccupationChange = event => {
+    setOccupation(event.target.value);
+    console.log("Changed")
+  }; 
 
-  const employeelist = employee.map((employee) =>
+  useEffect(() => {
+    setFilteredEmployees(
+      employees.filter(emp => emp.name.toLowerCase().includes(search.toLowerCase()))
+    )
+  }, [search, employees])
+
+  useEffect(() => {
+    setFilteredEmployees(
+      employees.filter(emp => emp.occupation.toLowerCase().includes(occupation.toLowerCase()))
+    )
+  }, [occupation, employees])
+
+  //const filteredEmployees = employees.filter(emp => emp.name.toLowerCase().includes(search.toLowerCase()));
+  
+
+
+  const employeelist = filteredEmployees.map((employee) =>
     <EmployeeCard
     key={employee.id}
     id={employee.id}
@@ -18,14 +44,14 @@ function App() {
     image={employee.image}
     occupation={employee.occupation}
     location={employee.location}
-    onClick={'remove employee'}
     />
     );
 
   return (
     <>
       <Title>Employee Directory</Title>
-      <SearchBar />
+      <DropDown employees={employees} onChange={handleOccupationChange} value={occupation}/><SearchBar value={search}
+        onChange={handleChange} />
     <Wrapper>
       {employeelist}
     </Wrapper>
