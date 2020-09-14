6da55ba1c4459d2d98ae4f6c739bca437c3a9db5
Lukey <ldipaola66@gmail.com>
2020-09-14 22:42:03 +1000

    added props to SearchBar

--- src/compenents/SearchBar/index.js
+++ src/compenents/SearchBar/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 
-function SearchBar() {
+function SearchBar(props) {
     return (
         <div>
             <Form.Group>
-            <Form.Control className="search" size="lg" type="text" placeholder="Search.." /> 
+            <Form.Control className="search" size="lg" type="text" placeholder="Search.." onChange={props.onChange} value={props.value} /> 
             </Form.Group>
         </div>
     )
