fbe43cf0563fb9272930bc0fd45cc88cad6324a4
Lukey <ldipaola66@gmail.com>
2020-09-19 22:18:57 +1000

    add sort button

--- src/App.js
+++ src/App.js
@@ -1,64 +1,109 @@
-import React, {useState, useEffect} from 'react';
-import Employees from './employees.json';
-import EmployeeCard from './compenents/EmployeeCard'
-import Wrapper from './compenents/Wrapper'
-import Title from './compenents/Title'
-import SearchFilter from './compenents/SearchFilter'
+import React, { useState, useEffect } from "react";
+import Employees from "./employees.json";
+import EmployeeCard from "./compenents/EmployeeCard";
+import Wrapper from "./compenents/Wrapper";
+import Title from "./compenents/Title";
+import SearchFilter from "./compenents/SearchFilter";
 
 function App() {
   const [employees, setEmployees] = useState(Employees);
   const [search, setSearch] = useState("");
   const [occupation, setOccupation] = useState("");
   const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [sortedEmployees, setSortedEmployees] = useState([]);
+  const [sortButton, setSortButton] = useState("Ascending");
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setSearch(event.target.value);
-  }; 
-  const handleOccupationChange = event => {
+  };
+  const handleOccupationChange = (event) => {
     setOccupation(event.target.value);
-    console.log("Changed")
-  }; 
+    console.log("Changed");
+  };
+
+  const handleClick = (event) => {
+    if (sortButton === "Descending") {
+      setSortedEmployees(
+        employees.sort((a, b) => {
+          let nameA = a.name.toUpperCase();
+          let nameB = b.name.toUpperCase(); 
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        })
+      );
+      setFilteredEmployees(sortedEmployees);
+      setSortButton("Ascending");
+    } else {
+      setSortedEmployees(
+        employees.sort((a, b) => {
+          let nameA = a.name.toUpperCase();
+          let nameB = b.name.toUpperCase(); 
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }).reverse()
+      );
+      setFilteredEmployees(sortedEmployees);
+       setSortButton("Descending");
+
+      }
+      console.log(sortedEmployees)
+    console.log(sortButton);
+  };
 
   useEffect(() => {
     setFilteredEmployees(
-      employees.filter(emp => emp.name.toLowerCase().includes(search.toLowerCase()))
-    )
-  }, [search, employees])
-
-  useEffect(() => {
-    setFilteredEmployees(
-      employees.filter(emp => emp.occupation.toLowerCase().includes(occupation.toLowerCase()))
-    )
-  }, [occupation, employees])
-
-  //const filteredEmployees = employees.filter(emp => emp.name.toLowerCase().includes(search.toLowerCase()));
-  
-
-
-  const employeelist = filteredEmployees.map((employee) =>
-    <EmployeeCard
-    key={employee.id}
-    id={employee.id}
-    name={employee.name}
-    image={employee.image}
-    occupation={employee.occupation}
-    location={employee.location}
-    />
+      employees.filter((emp) =>
+        emp.name.toLowerCase().includes(search.toLowerCase())
+      )
     );
+  }, [search, employees]);
+
+  useEffect(() => {
+    setFilteredEmployees(
+      employees.filter((emp) =>
+        emp.occupation.toLowerCase().includes(occupation.toLowerCase())
+      )
+    );
+  }, [occupation, employees]);
+
+  useEffect(() => {
+    setSortedEmployees(employees.sort());
+  }, [sortedEmployees, employees]);
+
+  const employeelist = filteredEmployees.map((employee) => (
+    <EmployeeCard
+      key={employee.id}
+      id={employee.id}
+      name={employee.name}
+      image={employee.image}
+      occupation={employee.occupation}
+      location={employee.location}
+    />
+  ));
 
   return (
     <>
       <Title>Meet Our Employees</Title>
-      <SearchFilter 
-      employees={employees} 
-      handleOccupationChange={handleOccupationChange} 
-      occupationValue={occupation} 
-      searchValue={search} 
-      handleSearchonChange={handleChange}
+      <SearchFilter
+        employees={employees}
+        handleOccupationChange={handleOccupationChange}
+        occupationValue={occupation}
+        searchValue={search}
+        sortButtonValue={sortButton}
+        handleSearchonChange={handleChange}
+        onClick={handleClick}
       />
-    <Wrapper>
-      {employeelist}
-    </Wrapper>
+      <Wrapper>{employeelist}</Wrapper>
     </>
   );
 }
--- src/compenents/SearchFilter/index.js
+++ src/compenents/SearchFilter/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function SearchFilter(props) {
   const options = props.employees.map((option) => 
@@ -13,12 +14,13 @@ function SearchFilter(props) {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Form.Group className="mb-0"  controlId="exampleForm.ControlSelect1">
+      <Form.Group className="mb-0"  controlId="occupation-select">
       <Form.Control as="select" onChange={props.handleOccupationChange} value={props.occupationValue}>
       <option value="">-- Occupation --</option>
       {options}
     </Form.Control>
     </Form.Group>
+    <Button onClick={props.onClick} style={{marginLeft: "1em"}} variant="primary">{props.sortButtonValue}</Button>{' '}
     </Nav>
     <Form inline>
     <Form.Control className="mr-sm-2" type="text" placeholder="Search.." onChange={props.handleSearchonChange} value={props.value} />
