3e6e9282fa1ae2c4ef72778322c6cf9d2a38e45e
Lukey <ldipaola66@gmail.com>
2020-09-21 18:22:38 +1000

    Updates

---
+++ precache-manifest.c4ad9f57f7c795ff7b7a47cc441b4cf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23206439fd01cb08f3edf56c1f3b4e40",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "bac563aa5737e6e27c2e",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2eede3fd3f6b50cbf8ee",
+    "url": "/Employee-Directory/static/css/main.77b620c8.chunk.css"
+  },
+  {
+    "revision": "bac563aa5737e6e27c2e",
+    "url": "/Employee-Directory/static/js/2.0484dced.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Employee-Directory/static/js/2.0484dced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eede3fd3f6b50cbf8ee",
+    "url": "/Employee-Directory/static/js/main.7827b0b6.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  },
+  {
+    "revision": "dfc83a7cb7cfce9c0e1a03e10060429e",
+    "url": "/Employee-Directory/static/media/annie-spratt-QckxruozjRg-unsplash.dfc83a7c.jpg"
+  }
+]);
